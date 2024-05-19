68a245c60b5a5ae05a2d032f6b319e491c9357f2
winson2623 <winsonchen86@gmail.com>
2024-05-20 07:46:32 +0800

    completed for loops exercise

---
+++ forLoops/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = 0; i < names.Length; i++)
+{
+    if (names[i] == "David")
+    {
+        names[i] = "Sammy";
+    }
+}
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
+*/
+
+for (int i = 1; i < 101; i ++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine($"{i} FizzBuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine($"{i} Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine($"{i} Buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
