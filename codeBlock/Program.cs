9c09eff80795f458e826c01ae052bf75cddbf862
winson2623 <winsonchen86@gmail.com>
2024-05-19 10:58:12 +0800

    Completed code block exercise

---
+++ codeBlock/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+int value;
+
+if (true)
+{
+    value = 10;
+    Console.WriteLine($"Inside the code block: {value}");
+}
+
+Console.WriteLine($"Outside the code block: {value}");
+*/
+
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = false;
+
+foreach (int number in numbers)
+{
+    total += number;
+    if (number == 42)
+        found = true;
+}
+
+if(found)
+    Console.WriteLine("Set contains 42");
+    
+Console.WriteLine($"Total: {total}");
