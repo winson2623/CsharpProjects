6a727b08de206197945dd71bdcdfb21e07ea85b1
winson2623 <winsonchen86@gmail.com>
2024-05-19 10:21:12 +0800

    boolean exercise completed

---
+++ Boolean/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Random coin = new();
+int headsOrTails = coin.Next(0,2);
+Console.WriteLine($"{(headsOrTails == 1 ? "heads" : "tails")}");
+*/
+
+string permission = "Admin";
+int level = 53;
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Admin super user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+
+if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges");
+    }
+}
+
+if (!permission.Contains("Admin") && !permission.Contains("Manager"))
+{
+   Console.WriteLine("You do not have sufficient privileges"); 
+}
+
