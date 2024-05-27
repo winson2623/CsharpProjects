df31db1077401bf4583d0870bcdba4eb09a42652
winson2623 <winsonchen86@gmail.com>
2024-05-28 07:58:23 +0800

    completed alphanumeric data module

---
+++ alphanumericData/Program.cs
@@ -0,0 +1,70 @@
+﻿/*
+decimal price = 67.55m;
+decimal salePrice = 59.99m;
+
+string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
+
+yourDiscount += $"A discount of {((price - salePrice)/price):P2}!"; //inserted
+Console.WriteLine(yourDiscount);
+*/
+
+/*
+int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
+
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"   Shares: {productShares:N3} Product");
+Console.WriteLine($"     Sub Total: {subtotal:C}");
+Console.WriteLine($"           Tax: {taxPercentage:P2}");
+Console.WriteLine($"     Total Billed: {total:C}");
+*/
+
+/*
+string paymentId = "769C";
+string payeeName = "Mr. Stephen Ortega";
+string paymentAmount = "$5,000.00";
+
+var formattedLine = paymentId.PadRight(6);
+formattedLine += payeeName.PadRight(24);
+formattedLine += paymentAmount.PadLeft(10);
+
+Console.WriteLine("1234567890123456789012345678901234567890");
+Console.WriteLine(formattedLine);
+*/
+
+
+string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+Console.WriteLine($"Dear {customerName},\nAs a customer of our Magic Yield offering we are excited to tell you about a new financial product that would dramatically increase your return.");
+Console.WriteLine($"Currently, you own {currentShares:N2} shares at a return of {currentReturn:P2}.\n");
+Console.WriteLine($"Our new product, Glorious Future offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:C2}.\n");
+
+// Your logic here
+var comparison = currentProduct.PadRight(20);
+comparison += currentReturn.ToString("P2").PadRight(15);
+comparison += currentProfit.ToString("C2");
+
+//Could also write like this using Format()
+//comparison += String.Format("{0:P}", currentReturn).PadRight(10);
+//comparison += String.Format("{0:C}", currentProfit).PadRight(20);
+
+Console.WriteLine("Here's a quick comparison:\n");
+Console.WriteLine(comparison);
+
+var newComparison = newProduct.PadRight(20);
+newComparison += newReturn.ToString("P2").PadRight(15);
+newComparison += newProfit.ToString("C2");
+
+Console.WriteLine(newComparison);
