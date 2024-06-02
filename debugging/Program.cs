d377150c47a718d406b86fbdd15ac87a019b03e3
winson2623 <winsonchen86@gmail.com>
2024-06-02 10:05:53 +0800

    completed intro to debugging module

---
+++ debugging/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+string[] students = new string[] {"Sophia", "Nicolas", "Zahirah", "Jeong"};
+
+int studentCount = students.Length;
+
+Console.WriteLine("The final name is: " + students[studentCount]);
+*/
+
+string[] students = new string[] {"Sophia", "Nicolas", "Zahirah", "Jeong"};
+
+int studentCount = students.Length;
+
+//arrays count from zero, fixed [studentCount - 1] to debug
+Console.WriteLine("The final name is: " + students[studentCount - 1]);
